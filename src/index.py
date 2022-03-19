357278fba762a78f4b599c86ab274767be04232c
John Clark <daprogrammer1@gmail.com>
2022-03-19 22:16:44 +0800

    just little changes

--- src/index.py
+++ src/index.py
@@ -1,16 +1,18 @@
 from rps_tui import cmd
 from rps import play
 from colorama import Fore, init, Style
-from platform import system
+from platform import system, python_version
 from configparser import ConfigParser
 from console_colors import consolecolor
+from os import path
 
 # This file is the starting point of the entire game.
 
 config = ConfigParser()
-config.read('./settings.ini')
+
 init()
 sys = system()
+pyver = python_version()
 
 # Colorama Variables
 
@@ -24,22 +26,23 @@ red = Fore.RED
 # Main Startup
 
 while True:
-    print(green+"Welcome to rps-python!\n"+yel)
-    print("You're using",sys,"as of now."+consolecolor)
-    print("Wanna play? Type \'y\' to start the game or type \'n\' if you're exiting."+mag)
-    print("Type \"cmds\" for special commands."+consolecolor)
-    startup = input().lower()
+	print(green+"Welcome to rps-python!\n"+yel)
+	print("You're using",sys,"as of now."+Style.DIM)
+	print("Python Version:", pyver+consolecolor+bright)
+	print("Wanna play? Type \'y\' to start the game or type \'n\' if you're exiting."+mag)
+	print("Type \"cmds\" for special commands."+consolecolor)
+	startup = input().lower()
 
-    if startup == "y":
-        play()
-        break
-    elif startup == "n" or startup == 'exit':
-        print("Exiting...")
-        exit()
-    elif startup == "cmds":
-        cmd()
-        break
-    else:
-        print(red+"Try again.\n")
+	if startup == "y":
+		play()
+		break
+	elif startup == "n" or startup == 'exit':
+		print("Exiting...")
+		exit()
+	elif startup == "cmds":
+		cmd()
+		break
+	else:
+		print(red+"Try again.\n")
 
-# Version v1.0.4
+# Version v1.0.4a
